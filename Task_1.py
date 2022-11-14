4b4917b9dba648e82201b7113ca1f838a42f8cec
vivimila <violamilashevskaya@yandex.by>
2022-11-14 18:58:13 +0300

    work with game.add rulls

--- Task_1.py
+++ Task_1.py
@@ -9,4 +9,17 @@ def game_board():
         print("|", board[0 + i*3], "|", board[1 + i*3], "|", board[2 + i*3], "|")
     print("-------------")
 
-game_board()
+def input_an_action (entering):
+    while True:
+        value = input ("Сделайте ход. Выберите ячейку: " +entering)  
+        if not (value in "123456789"):
+            print("Неверные ввод.Повторитет попытку")
+            continue
+        value = int(value)
+        if str(board[value -1] in "XO"):
+            print("УПС! Место занято. Повторите попытку")
+            continue
+        board[value -1] = entering
+        break
+
+#game_board()
